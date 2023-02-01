189d7536cd52e9a0c9c71b0a045b796950e45c31
TomerChermesh <tomerchermesh@gmail.com>
2023-02-01 15:09:52 +0200

    🎉 Initial commit

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+from functools import lru_cache
+
+from dotenv import load_dotenv
+from pydantic import BaseSettings
+
+
+class Config(BaseSettings):
+    telegram_bot_token: str
+
+    class Config:
+
+        load_dotenv()
+
+        telegram_bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
+
+
+@lru_cache()
+def get_config():
+    return Config()
