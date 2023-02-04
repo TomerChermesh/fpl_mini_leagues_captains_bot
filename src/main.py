e7a1fac8448b4e218ccc742dc2d49118aae2c40b
TomerChermesh <tomerchermesh@gmail.com>
2023-02-04 14:30:33 +0200

    🚀 Run bot on localhost with bottle library

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
-from src.bot import Bot
+from bottle import run, post
 
-if __name__ == '__main__':
+from src.bot import Bot
+from src.config import get_config
+
+config = get_config()
+
+
+@post('/')
+def app_run():
     bot: Bot = Bot()
     bot.run()
+
+
+app_run()
+
+if __name__ == '__main__':
+    run(host='localhost', port=config.port, debug=True)
