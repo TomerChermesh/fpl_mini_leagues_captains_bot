5409d2cdd219f61eecb7a41aed9c3684dfb6623c
TomerChermesh <tomerchermesh@gmail.com>
2024-04-14 10:29:27 +0300

    fix

--- src/config.py
+++ src/config.py
@@ -7,14 +7,10 @@ from pydantic import BaseSettings
 
 class Config(BaseSettings):
     telegram_bot_token: str
-    port: int
 
     class Config:
-
         load_dotenv()
-
         telegram_bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
-        port = os.environ.get('PORT')
 
 
 @lru_cache()
--- src/main.py
+++ src/main.py
@@ -1,18 +1,10 @@
-from bottle import run, post
-
 from src.bot import Bot
-from src.config import get_config
-
-config = get_config()
 
 
-@post('/')
 def app_run():
     bot: Bot = Bot()
     bot.run()
 
 
-app_run()
-
 if __name__ == '__main__':
-    run(host='localhost', port=config.port, debug=True)
+    app_run()
